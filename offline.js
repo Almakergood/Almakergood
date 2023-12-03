fa2a88e64a854f489d155805c730827f01e8f4c1
Almakergood <49840366+Almakergood@users.noreply.github.com>
2023-12-03 17:34:26 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1701606623,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"divbg.png",
+		"divbg2.png",
+		"gameico-sheet0.png",
+		"gameico-sheet1.png",
+		"divbg3.png",
+		"gamescreenshot-sheet0.png",
+		"button.png",
+		"buttonsprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"def.png",
+		"styles.css",
+		"arial_curive.otf",
+		"catalog.xml",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
+	]
+}
